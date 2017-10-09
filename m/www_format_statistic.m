ca0631005c2ba26ea1590bbca7a7d8a3fd188c86
Jérôme KUNEGIS <kunegis@gmail.com>
2017-10-09 16:20:40 +0200

    formatting of integers and signed statistics

--- m/www_format_statistic.m
+++ m/www_format_statistic.m
@@ -1,11 +1,32 @@
 function text = www_format_statistic(statistic, value)
 
-text = sprintf('%g', value);
+[logarithmic integer percent negative] = konect_data_statistic();
 
+statistic_dataname = regexprep(statistic, '\+', '__'); 
+
+loga = logarithmic.(statistic_dataname);
+inte = integer.(statistic_dataname);
+perc = percent.(statistic_dataname); 
+nega = negative.(statistic_dataname); 
+
+if inte
+  if nega
+    text = sprintf('%+d', value); 
+  else
+    text = sprintf('%d', value);
+  end
+else
+  if nega
+    text = sprintf('%+g', value);
+  else
+    text = sprintf('%g', value);
+  end
+end
+  
 % Remove leading zeroes in the exponent
 text = regexprep(text, '(e[+-])0+', '$1'); 
 
-% Insert commas
+% Insert commas (integral part)
 text = regexprep(text, '^([^.]*[0-9])([0-9]{27}(\.|$))', '$1,$2'); 
 text = regexprep(text, '^([^.]*[0-9])([0-9]{24}(\.|$))', '$1,$2'); 
 text = regexprep(text, '^([^.]*[0-9])([0-9]{21}(\.|$))', '$1,$2'); 
@@ -16,8 +37,19 @@ text = regexprep(text, '^([^.]*[0-9])([0-9]{9}(\.|$))', '$1,$2');
 text = regexprep(text, '^([^.]*[0-9])([0-9]{6}(\.|$))', '$1,$2'); 
 text = regexprep(text, '^([^.]*[0-9])([0-9]{3}(\.|$))', '$1,$2'); 
 
-% Remove all plus signs
-text = regexprep(text, '\+', '');
+% Insert spaces (fractional part)
+text = regexprep(text, '(\.[0-9]{27})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{24})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{21})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{18})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{15})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{12})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{9})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{6})([0-9].*)$', '$1 $2'); 
+text = regexprep(text, '(\.[0-9]{3})([0-9].*)$', '$1 $2'); 
+
+% Remove the plus sign in the exponent
+text = regexprep(text, 'e\+', 'e');
 
 % Format minus signs
 text = regexprep(text, '-', '−');
